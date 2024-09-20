344bd39ba2b35a936bfe1ea3d9f323da023fd1ab
zuhri utama <zuhriutama@gmail.com>
2024-09-20 10:08:37 +0700

    update order detail

--- resources/views/order-detail.blade.php
+++ resources/views/order-detail.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.web')
 @section('content')
 <div class="min-h-screen bg-base-200">
-    <div class="container mx-auto px-4">
+    <div class="container mx-auto px-4 pt-10">
+        <div class="alert alert-success shadow-lg mb-10">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current h-6 w-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <span>Pemesanan telah dibuat. Kami akan segera mengonfirmasi pemesanan Anda.</span>
+            </div>
+        </div>
         <div class="bg-white rounded-lg shadow-md p-6">
             <h2 class="text-xl font-semibold mb-4">Order Details</h2>
             <div class="flex flex-col gap-4">
